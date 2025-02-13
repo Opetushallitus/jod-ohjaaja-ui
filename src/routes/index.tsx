08f720c1a2ebc7cde7c791460b0d1e754e96fa39
Jussi Sirén <jussi.siren@sleek.works>
2025-02-13 11:01:35 +0200

    OPHJOD-1253: Add content listing view

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import i18n, { supportedLanguageCodes } from '@/i18n/config';
 import { ContentDetails, contentDetailsLoader } from '@/routes/ContentDetails';
 import { RouteObject, replace } from 'react-router';
+import { CategoryContent } from './CategoryContent';
 import { Home, homeLoader } from './Home';
 import { NoMatch, Root, rootLoader } from './Root';
 
@@ -9,6 +10,12 @@ const contentDetailsRoutes: RouteObject[] = supportedLanguageCodes.map((lng) => 
   path: `${i18n.t('slugs.content-details', { lng })}/:id`,
   element: <ContentDetails />,
   loader: contentDetailsLoader,
+}));
+
+const informationResourcesRoutes: RouteObject[] = supportedLanguageCodes.map((lng) => ({
+  id: `{slugs.information-resources}|${lng}`,
+  path: `${i18n.t('slugs.information-resources', { lng })}`,
+  element: <CategoryContent />,
 }));
 
 const rootRoute: RouteObject = {
@@ -22,6 +29,7 @@ const rootRoute: RouteObject = {
       element: <Home />,
       loader: homeLoader,
     },
+    ...informationResourcesRoutes,
     ...contentDetailsRoutes,
   ],
 };
