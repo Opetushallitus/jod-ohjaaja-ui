c205daeaf041a8af0a2af39bc35ce1c54c60d029
Sauli Anto <sauli.anto@gofore.com>
2025-02-17 17:05:12 +0200

    OPHJOD-1289: Add FeatureCard and update home view

--- src/routes/Root/Root.tsx
+++ src/routes/Root/Root.tsx
@@ -67,7 +67,7 @@ const Root = () => {
   }, [i18n.language]);
 
   return (
-    <div className="bg-bg-gray">
+    <>
       <link rel="manifest" href={`/ohjaaja/manifest-${language}.json`} crossOrigin="use-credentials" />
       <header role="banner" className="sticky top-0 z-30 print:hidden">
         <SkipLink hash="#jod-main" label={t('skiplinks.main')} />
@@ -135,7 +135,7 @@ const Root = () => {
         className={!sm ? 'py-7' : undefined}
       />
       <ScrollRestoration />
-    </div>
+    </>
   );
 };
 
