1ba44f8db252755213e96e9fc4dd570c3a29cd3b
Jussi Sirén <jussi.siren@sleek.works>
2025-05-09 15:25:34 +0300

    OPHJOD-1586: add SearchBanner

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -1,29 +1,34 @@
 import React from 'react';
 import { Breadcrumb } from '../Breadcrumb/Breadcrumb';
+import { SearchBanner } from '../SearchBanner/SearchBanner';
 
 interface MainLayoutProps {
   children: React.ReactNode;
+  hideSearch?: boolean;
   navChildren?: React.ReactNode;
 }
 
-export const MainLayout = ({ children, navChildren }: MainLayoutProps) => {
+export const MainLayout = ({ children, hideSearch, navChildren }: MainLayoutProps) => {
   return (
-    <div className="mx-auto grid w-full max-w-[1140px] grow grid-cols-3 gap-6 px-5 pb-6 pt-5 sm:px-6 print:p-0 auto-rows-max">
-      <Breadcrumb />
+    <>
+      {!hideSearch && <SearchBanner />}
+      <div className="mx-auto grid w-full max-w-[1140px] grow grid-cols-3 gap-6 px-5 pb-6 pt-5 sm:px-6 print:p-0 auto-rows-max">
+        <Breadcrumb />
 
-      {navChildren && (
-        <aside className="lg:order-last col-span-3 lg:col-span-1 print:hidden position-relative lg:position-static z-10 lg:z-auto h-[47px] lg:h-auto">
-          <nav
-            role="navigation"
-            className="sticky position-absolute top-0 left-0 w-full lg:top-[96px] lg:position-static max-h-[calc(100vh-196px)] overflow-y-auto scrollbar-hidden "
-          >
-            {navChildren}
-          </nav>
-        </aside>
-      )}
-      <main role="main" className="col-span-3 lg:col-span-2 print:col-span-3" id="jod-main">
-        {children}
-      </main>
-    </div>
+        {navChildren && (
+          <aside className="lg:order-last col-span-3 lg:col-span-1 print:hidden position-relative lg:position-static z-10 lg:z-auto h-[47px] lg:h-auto">
+            <nav
+              role="navigation"
+              className="sticky position-absolute top-0 left-0 w-full lg:top-[96px] lg:position-static max-h-[calc(100vh-196px)] overflow-y-auto scrollbar-hidden"
+            >
+              {navChildren}
+            </nav>
+          </aside>
+        )}
+        <main role="main" className="col-span-3 lg:col-span-2 print:col-span-3" id="jod-main">
+          {children}
+        </main>
+      </div>
+    </>
   );
 };
--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -3,6 +3,7 @@ import { ArticleCard } from '@/components/ArticleCard/ArticleCard';
 import { FeatureCard } from '@/components/FeatureCard/FeatureCard';
 import { LoginBanner } from '@/components/LoginBanner/LoginBanner';
 import { RecentlyWatchedContent } from '@/components/RecentlyWatchedContent/RecentlyWatchedContent';
+import { SearchBanner } from '@/components/SearchBanner/SearchBanner';
 import { LangCode } from '@/i18n/config';
 import { LoaderData } from '@/routes/Home/loader';
 import { getMainCategoryPath } from '@/utils/navigation-paths';
@@ -30,122 +31,125 @@ const Home = () => {
   }, [data, isLoggedIn]);
 
   return (
-    <main id="jod-main" className="w-full max-w-(--breakpoint-xl) mx-auto">
-      <title>{t('osaamispolku')}</title>
+    <>
+      <SearchBanner />
+      <main id="jod-main" className="w-full max-w-(--breakpoint-xl) mx-auto">
+        <title>{t('osaamispolku')}</title>
 
-      <img
-        src={heroSrc}
-        alt=""
-        role="none"
-        className="absolute w-(--breakpoint-xl) h-[320px] md:h-[454px] object-cover object-object-[50%] lg:object-[0_20%] -z-10"
-      />
+        <img
+          src={heroSrc}
+          alt=""
+          role="none"
+          className="absolute w-(--breakpoint-xl) h-[320px] md:h-[454px] object-cover object-object-[50%] lg:object-[0_20%] -z-10"
+        />
 
-      <div className="grid gap-8 grid-cols-3 max-w-[1140px] mx-auto px-5 sm:px-6 pt-[186px] md:pt-[72px] pb-7 md:pb-[40px] lg:pb-[75px]">
-        <div className="col-span-3 flex flex-col gap-3 md:gap-5 lg:gap-8">
-          <FeatureCard
-            level="h1"
-            hero
-            title={t('home.card-1-title')}
-            content={t('home.card-1-content')}
-            backgroundColor="#00A8B3BF"
-            className="md:w-min"
-          />
-          <div className="flex flex-col lg:flex-row gap-3 md:gap-5 xl:gap-7">
+        <div className="grid gap-8 grid-cols-3 max-w-[1140px] mx-auto px-5 sm:px-6 pt-[186px] md:pt-[72px] pb-7 md:pb-[40px] lg:pb-[75px]">
+          <div className="col-span-3 flex flex-col gap-3 md:gap-5 lg:gap-8">
             <FeatureCard
-              to={getMainCategoryPath(language as LangCode, 0)}
-              linkComponent={Link}
-              level="h2"
-              title={t('home.card-2-title')}
-              content={t('home.card-2-content')}
-              backgroundColor="var(--ds-color-secondary-1)"
-              collapseOnSmallScreen={true}
-              opacity={0.95}
-              className="flex-1"
+              level="h1"
+              hero
+              title={t('home.card-1-title')}
+              content={t('home.card-1-content')}
+              backgroundColor="#00A8B3BF"
+              className="md:w-min"
             />
-            <FeatureCard
-              to={getMainCategoryPath(language as LangCode, 1)}
-              linkComponent={Link}
-              level="h2"
-              title={t('home.card-3-title')}
-              content={t('home.card-3-content')}
-              backgroundColor="var(--ds-color-secondary-3)"
-              collapseOnSmallScreen={true}
-              opacity={0.95}
-              className="flex-1"
-            />
-            <FeatureCard
-              to={getMainCategoryPath(language as LangCode, 2)}
-              linkComponent={Link}
-              level="h2"
-              title={t('home.card-4-title')}
-              content={t('home.card-4-content')}
-              backgroundColor="var(--ds-color-secondary-4)"
-              collapseOnSmallScreen={true}
-              opacity={0.95}
-              className="flex-1"
+            <div className="flex flex-col lg:flex-row gap-3 md:gap-5 xl:gap-7">
+              <FeatureCard
+                to={getMainCategoryPath(language as LangCode, 0)}
+                linkComponent={Link}
+                level="h2"
+                title={t('home.card-2-title')}
+                content={t('home.card-2-content')}
+                backgroundColor="var(--ds-color-secondary-1)"
+                collapseOnSmallScreen={true}
+                opacity={0.95}
+                className="flex-1"
+              />
+              <FeatureCard
+                to={getMainCategoryPath(language as LangCode, 1)}
+                linkComponent={Link}
+                level="h2"
+                title={t('home.card-3-title')}
+                content={t('home.card-3-content')}
+                backgroundColor="var(--ds-color-secondary-3)"
+                collapseOnSmallScreen={true}
+                opacity={0.95}
+                className="flex-1"
+              />
+              <FeatureCard
+                to={getMainCategoryPath(language as LangCode, 2)}
+                linkComponent={Link}
+                level="h2"
+                title={t('home.card-4-title')}
+                content={t('home.card-4-content')}
+                backgroundColor="var(--ds-color-secondary-4)"
+                collapseOnSmallScreen={true}
+                opacity={0.95}
+                className="flex-1"
+              />
+            </div>
+          </div>
+          <div className="col-span-3">
+            <h2 className="text-heading-2-mobile sm:text-heading-2 mb-5">{t('home.new-content')}</h2>
+            <CardCarousel
+              itemWidth={261}
+              items={carouselItems}
+              translations={{
+                prevTrigger: t('carousel.prev'),
+                nextTrigger: t('carousel.next'),
+                indicator: (index: number) => t('carousel.indicator', { index: index + 1 }),
+              }}
+              className="max-[640px]:px-5 max-[640px]:-mx-5 max-[1148px]:px-6 max-[1148px]:-mx-6 p-3 -m-3"
             />
           </div>
-        </div>
-        <div className="col-span-3">
-          <h2 className="text-heading-2-mobile sm:text-heading-2 mb-5">{t('home.new-content')}</h2>
-          <CardCarousel
-            itemWidth={261}
-            items={carouselItems}
-            translations={{
-              prevTrigger: t('carousel.prev'),
-              nextTrigger: t('carousel.next'),
-              indicator: (index: number) => t('carousel.indicator', { index: index + 1 }),
-            }}
-            className="max-[640px]:px-5 max-[640px]:-mx-5 max-[1148px]:px-6 max-[1148px]:-mx-6 p-3 -m-3"
-          />
-        </div>
 
-        {isLoggedIn && (
-          <div className="col-span-3 lg:col-span-2">
-            <FeatureCard
-              to="/"
-              linkComponent={Link}
-              level="h2"
-              title={t('home.favorites')}
-              content={t('home.favorites-content')}
-              backgroundColor="#85C4EC"
-            />
-          </div>
-        )}
-
-        <div className="col-span-3">
-          <h2 className="text-heading-2-mobile sm:text-heading-2 mb-5">{t('home.popular-content')}</h2>
-          <CardCarousel
-            itemWidth={261}
-            items={carouselItems}
-            translations={{
-              prevTrigger: t('carousel.prev'),
-              nextTrigger: t('carousel.next'),
-              indicator: (index: number) => t('carousel.indicator', { index: index + 1 }),
-            }}
-            className="max-[640px]:px-5 max-[640px]:-mx-5 max-[1148px]:px-6 max-[1148px]:-mx-6 p-3 -m-3"
-          />
-        </div>
-
-        {isLoggedIn ? (
-          <div className="col-span-3 grid grid-cols-3 gap-x-6 xl:gap-x-7">
-            <RecentlyWatchedContent />
-            <div className="col-span-3 lg:col-span-1 content-end">
+          {isLoggedIn && (
+            <div className="col-span-3 lg:col-span-2">
               <FeatureCard
                 to="/"
                 linkComponent={Link}
                 level="h2"
-                title={t('suggest-new-content-for-the-service')}
-                content={t('suggest-new-content-for-the-service-content')}
-                backgroundColor="#EBB8E1"
+                title={t('home.favorites')}
+                content={t('home.favorites-content')}
+                backgroundColor="#85C4EC"
               />
             </div>
+          )}
+
+          <div className="col-span-3">
+            <h2 className="text-heading-2-mobile sm:text-heading-2 mb-5">{t('home.popular-content')}</h2>
+            <CardCarousel
+              itemWidth={261}
+              items={carouselItems}
+              translations={{
+                prevTrigger: t('carousel.prev'),
+                nextTrigger: t('carousel.next'),
+                indicator: (index: number) => t('carousel.indicator', { index: index + 1 }),
+              }}
+              className="max-[640px]:px-5 max-[640px]:-mx-5 max-[1148px]:px-6 max-[1148px]:-mx-6 p-3 -m-3"
+            />
           </div>
-        ) : (
-          <LoginBanner />
-        )}
-      </div>
-    </main>
+
+          {isLoggedIn ? (
+            <div className="col-span-3 grid grid-cols-3 gap-x-6 xl:gap-x-7">
+              <RecentlyWatchedContent />
+              <div className="col-span-3 lg:col-span-1 content-end">
+                <FeatureCard
+                  to="/"
+                  linkComponent={Link}
+                  level="h2"
+                  title={t('suggest-new-content-for-the-service')}
+                  content={t('suggest-new-content-for-the-service-content')}
+                  backgroundColor="#EBB8E1"
+                />
+              </div>
+            </div>
+          ) : (
+            <LoginBanner />
+          )}
+        </div>
+      </main>
+    </>
   );
 };
 
