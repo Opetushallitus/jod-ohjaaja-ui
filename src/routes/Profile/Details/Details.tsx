4ab36fb745dd4f85e8acdf33e70e034533a1c188
Jussi Sirén <jussi.siren@sleek.works>
2025-06-30 13:43:25 +0300

    OPHJOD-1602: Add workplace selection

--- src/routes/Profile/Details/Details.tsx
+++ src/routes/Profile/Details/Details.tsx
@@ -1,3 +1,5 @@
+import { client } from '@/api/client';
+import { components } from '@/api/schema';
 import { MainLayout } from '@/components';
 import { ProfileNavigation } from '@/components/MainLayout/ProfileNavigation';
 import { useTags } from '@/hooks/useTags';
@@ -9,13 +11,30 @@ import { useLoaderData } from 'react-router';
 import { useShallow } from 'zustand/react/shallow';
 import { LoaderData } from './loader';
 
+const isTyoskentelyPaikka = (
+  value: string | undefined,
+): value is components['schemas']['OhjaajaDto']['tyoskentelyPaikka'] => {
+  return [
+    'PERUSASTE',
+    'TOINEN_ASTE',
+    'KORKEAKOULU',
+    'AIKUISKOULUTUS',
+    'TYOLLISYYSPALVELUT',
+    'KOLMAS_SEKTORI',
+    'YKSITYINEN',
+    'MUU',
+    undefined,
+  ].includes(value);
+};
+
 const Details = () => {
   const { t } = useTranslation();
-  const { firstName, lastName } = useLoaderData<LoaderData>();
+  const { firstName, lastName, tyoskentelyPaikka } = useLoaderData<LoaderData>();
   const { tags } = useTags();
   const [kiinnostukset, toggleKiinnostus] = useKiinnostuksetStore(
     useShallow((state) => [state.kiinnostukset, state.toggleKiinnostus]),
   );
+  const [currentTyoskentelyPaikka, setCurrentTyoskentelyPaikka] = React.useState(tyoskentelyPaikka);
 
   const isSelected = React.useCallback(
     (tagId: number) => {
@@ -28,6 +47,13 @@ const Details = () => {
     toggleKiinnostus(Number(event.target.value));
   };
 
+  const handleWorkplaceChange = async (value: string) => {
+    if (isTyoskentelyPaikka(value)) {
+      await client.PUT('/api/profiili/ohjaaja', { body: { tyoskentelyPaikka: value } });
+      setCurrentTyoskentelyPaikka(value);
+    }
+  };
+
   return (
     <MainLayout navChildren={<ProfileNavigation />}>
       <title>{t('profile.details.title')}</title>
@@ -38,14 +64,24 @@ const Details = () => {
             lastName,
           })}
         </h1>
-        <section className="mb-8 bg-todo">
-          TODO
+        <section className="mb-8">
           <h2 className="text-heading-2 mb-6">{t('profile.details.introduction.title')}</h2>
           <p className="text-body-md mb-6">{t('profile.details.introduction.description')}</p>
           <Combobox
-            label={t('profile.details.introduction.label')}
-            placeholder={t('profile.details.introduction.placeholder')}
-            options={[]}
+            label={t('profile.details.workplace.label')}
+            placeholder={t('profile.details.workplace.placeholder')}
+            selected={currentTyoskentelyPaikka ?? ''}
+            options={[
+              { value: 'PERUSASTE', label: t('profile.details.workplace.option1') },
+              { value: 'TOINEN_ASTE', label: t('profile.details.workplace.option2') },
+              { value: 'KORKEAKOULU', label: t('profile.details.workplace.option3') },
+              { value: 'AIKUISKOULUTUS', label: t('profile.details.workplace.option4') },
+              { value: 'TYOLLISYYSPALVELUT', label: t('profile.details.workplace.option5') },
+              { value: 'KOLMAS_SEKTORI', label: t('profile.details.workplace.option6') },
+              { value: 'YKSITYINEN', label: t('profile.details.workplace.option7') },
+              { value: 'MUU', label: t('profile.details.workplace.option8') },
+            ]}
+            onChange={handleWorkplaceChange}
           />
         </section>
         <section>
--- src/routes/Profile/Details/loader.ts
+++ src/routes/Profile/Details/loader.ts
@@ -1,9 +1,13 @@
+import { client } from '@/api/client';
 import { getKiinnostukset } from '@/api/kiinnostukset';
 import { LoaderFunction } from 'react-router';
 
 const loader = (async ({ context }) => {
-  const kiinnostukset = await getKiinnostukset();
-  return { kiinnostukset, firstName: context?.etunimi, lastName: context?.sukunimi };
+  const [kiinnostukset, tyoskentelyPaikka] = await Promise.all([
+    getKiinnostukset(),
+    (await client.GET('/api/profiili/ohjaaja')).data?.tyoskentelyPaikka,
+  ]);
+  return { kiinnostukset, firstName: context?.etunimi, lastName: context?.sukunimi, tyoskentelyPaikka };
 }) satisfies LoaderFunction;
 export type LoaderData = Awaited<ReturnType<typeof loader>>;
 export default loader;
