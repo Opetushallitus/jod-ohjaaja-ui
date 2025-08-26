d51924d1a054de2a91102105a3f24759cc64a0ca
Jussi Sirén <jussi.siren@sleek.works>
2025-08-26 15:24:52 +0300

    OPHJOD-1921: Update Navigation bar styles

--- src/components/LanguageButton/LanguageButton.tsx
+++ src/components/LanguageButton/LanguageButton.tsx
@@ -1,4 +1,5 @@
 import { langLabels } from '@/i18n/config';
+import { useMediaQueries } from '@jod/design-system';
 import { JodCaretDown, JodCaretUp, JodLanguage } from '@jod/design-system/icons';
 import { useTranslation } from 'react-i18next';
 import { LanguageMenu } from '../LanguageMenu/LanguageMenu';
@@ -15,16 +16,23 @@ export const LanguageButton = ({ onClick, langMenuOpen, menuRef, onMenuBlur, onM
   const {
     i18n: { language: languageKey },
   } = useTranslation();
+
+  const { sm } = useMediaQueries();
+
+  const caret = sm ? <>{langMenuOpen ? <JodCaretUp size={20} /> : <JodCaretDown size={20} />}</> : null;
+
   return (
     <div className="relative">
-      <button onClick={onClick} className="flex gap-2 justify-center items-center select-none cursor-pointer">
-        <span className="size-7 flex justify-center items-center">
-          <JodLanguage />
+      <button
+        onClick={onClick}
+        className="flex flex-col sm:flex-row justify-center items-center select-none cursor-pointer sm:mr-5"
+      >
+        <JodLanguage className="mx-auto" />
+
+        <span className="whitespace-nowrap text-[12px] sm:text-button-sm sm:mx-3">
+          {langLabels[languageKey as keyof typeof langLabels]}
         </span>
-        <span className="py-3 whitespace-nowrap">{langLabels[languageKey as keyof typeof langLabels]}</span>
-        <span className="size-7 flex justify-center items-center">
-          {langMenuOpen ? <JodCaretUp /> : <JodCaretDown />}
-        </span>
+        {caret}
       </button>
       {langMenuOpen && (
         <div ref={menuRef} onBlur={onMenuBlur} className="absolute right-0 translate-y-8">
