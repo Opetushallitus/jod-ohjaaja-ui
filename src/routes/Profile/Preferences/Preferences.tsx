63387bd99ca623e13c6e4d3060a5e66dcf7ac1ed
Jussi Sirén <jussi.siren@sleek.works>
2025-09-05 10:17:04 +0300

    OPHJOD-1990: Remove unused data disclosure section and related components from Preferences

--- src/routes/Profile/Preferences/Preferences.tsx
+++ src/routes/Profile/Preferences/Preferences.tsx
@@ -1,24 +1,13 @@
 import { MainLayout } from '@/components';
 import { ProfileNavigation } from '@/components/MainLayout/ProfileNavigation';
 import { LogoutFormContext } from '@/routes/Root';
-import { Button, ConfirmDialog, Toggle } from '@jod/design-system';
+import { Button, ConfirmDialog } from '@jod/design-system';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 const DownloadLink = ({ children }: { children: React.ReactNode }) => (
   <a href={`${import.meta.env.BASE_URL}api/profiili/ohjaaja/vienti`}>{children}</a>
 );
-
-const disclosureOfData = [
-  'teaching-evaluation',
-  'evaluation-of-education',
-  'competence-assessment',
-  'research',
-  'development',
-  'statistics-and-monitoring',
-  'guidance-and-planning',
-  'archiving',
-];
 
 const Preferences = () => {
   const { t } = useTranslation();
@@ -40,28 +29,6 @@ const Preferences = () => {
         <h1 className="mb-5 text-heading-2 sm:text-heading-1" data-testid="preferences-title">
           {t('profile.preferences.title')}
         </h1>
-
-        <div className="mb-8 text-body-md flex flex-col gap-7" data-testid="preferences-description">
-          <p>{t('profile.preferences.description')}</p>
-        </div>
-        <section className="mb-8" data-testid="preferences-disclosure">
-          <h3 className="mb-3 text-heading-3">{t('profile.preferences.disclosure-of-data.title')}</h3>
-          <p className="text-body-md mb-5">{t('profile.preferences.disclosure-of-data.description')}</p>
-          <div className="grid grid-cols-1 gap-5 bg-todo">
-            <p>TODO</p>
-            {disclosureOfData.map((data, index) => (
-              <React.Fragment key={data}>
-                <SettingsRow
-                  title={t(`profile.preferences.disclosure-of-data.${data}.title`)}
-                  description={t(`profile.preferences.disclosure-of-data.${data}.description`)}
-                  isSelected={false}
-                  onChange={() => {}}
-                />
-                {index < disclosureOfData.length - 1 && <hr className="border-border-gray" />}
-              </React.Fragment>
-            ))}
-          </div>
-        </section>
         <section className="mb-8" data-testid="preferences-download">
           <h2 className="text-heading-2-mobile sm:text-heading-2 mb-3">{t('profile.preferences.download.title')}</h2>
           <p className="text-body-md mb-5">{t('profile.preferences.download.description')}</p>
@@ -99,27 +66,5 @@ const Preferences = () => {
     </MainLayout>
   );
 };
-interface SettingsRowProps {
-  title: string;
-  description: string;
-  isSelected: boolean;
-  onChange: () => void;
-}
-
-const SettingsRow = ({ title, description, isSelected, onChange }: SettingsRowProps) => (
-  <div className="grid grid-cols-2 items-center place-items-end" data-testid="preferences-setting-row">
-    <div className="place-self-start">
-      <h4 className="text-heading-4 mb-2">{title}</h4>
-      <p className="text-body-md">{description}</p>
-    </div>
-    <Toggle
-      serviceVariant="ohjaaja"
-      ariaLabel={title}
-      onChange={onChange}
-      checked={isSelected}
-      data-testid="preferences-setting-toggle"
-    ></Toggle>
-  </div>
-);
 
 export default Preferences;
