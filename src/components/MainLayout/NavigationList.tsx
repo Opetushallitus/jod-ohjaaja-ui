ca11f9aad4e996b6d6cd1f813d4a1276aed2b8a8
Jussi Sirén <jussi.siren@sleek.works>
2025-10-02 14:07:59 +0300

    OPHJOD-2412: Refactor page navigation component and tag list container

--- src/components/MainLayout/NavigationList.tsx
+++ src/components/MainLayout/NavigationList.tsx
@@ -1,4 +1,4 @@
-import { cx } from '@jod/design-system';
+import { tidyClasses as tc } from '@jod/design-system';
 import { useTranslation } from 'react-i18next';
 import { NavLink } from 'react-router';
 
@@ -11,52 +11,70 @@ type NavigationListItem = {
 
 type NavigationListProps = {
   rootItem: NavigationListItem;
+  menuTitle?: string;
 };
 
-const LinkOrSpan = ({ item, level }: { item: NavigationListItem; level: 1 | 2 }) => {
+const LinkOrSpan = ({ item }: { item: NavigationListItem }) => {
   const { i18n } = useTranslation();
   return item.path ? (
     <NavLink
       to={item.path}
       lang={i18n.language}
       className={({ isActive }) =>
-        cx(
-          `hyphens-auto text-black w-full block py-3 text-button-md hover:underline ${level === 2 ? 'pl-7' : 'pl-5'}`,
-          {
-            'bg-secondary-2-50 rounded-md': isActive,
-          },
-        )
+        tc([
+          'hyphens-auto',
+          'flex-1',
+          'flex',
+
+          'p-3',
+          'gap-3',
+          'items-center',
+          'text-button-md',
+          'rounded',
+          'cursor-pointer',
+          'hover:underline',
+          'active:underline',
+          'active:text-white',
+          'active:bg-secondary-2-dark-2',
+          !isActive ? 'hover:bg-bg-gray' : '',
+          isActive ? 'bg-secondary-2-dark text-white' : '',
+        ])
       }
       end
-      data-testid={`navlist-link-${level}`}
+      data-testid={`navlist-link-${item.id}`}
     >
       {item.title}
     </NavLink>
   ) : (
     <span
-      className={`hyphens-auto text-black w-full block py-3 text-button-md ${level === 2 ? 'pl-7' : 'pl-5'}`}
-      data-testid={`navlist-text-${level}`}
+      className={'hyphens-auto flex items-center text-[14px] leading-[20px] flex-1 p-3'}
+      data-testid={`navlist-text-${item.id}`}
     >
       {item.title}
     </span>
   );
 };
 
-export const NavigationList = ({ rootItem }: NavigationListProps) => {
+export const NavigationList = ({ rootItem, menuTitle }: NavigationListProps) => {
   return (
-    <ul className="flex flex-col gap-y-2 py-3" data-testid="navigation-list">
-      <li className="flex min-h-7 items-center w-full" data-testid="navigation-list-root">
-        <LinkOrSpan item={rootItem} level={1} />
-      </li>
-      {rootItem.children?.map((category) => (
-        <li
-          key={category.id}
-          className="flex min-h-7 items-center w-full"
-          data-testid={`navigation-list-item-${category.id}`}
-        >
-          <LinkOrSpan item={category} level={2} />
+    <div data-testid={`navigation-list-container`}>
+      {menuTitle && <span className="text-body-sm mb-4 mt-2 flex">{menuTitle}</span>}
+      <ul className="flex flex-col gap-3" data-testid="navigation-list">
+        <li data-testid="navigation-list-root">
+          <div className={tc(`flex flex-row space-between min-h-8 gap-2 mb-2`)}>
+            <LinkOrSpan item={rootItem} />
+          </div>
+          <ul className="flex flex-col gap-3 ml-5" data-testid="navigation-list-children">
+            {rootItem.children?.map((category) => (
+              <li key={category.id} data-testid={`navigation-list-item-${category.id}`}>
+                <div className={tc(`flex flex-row space-between min-h-8 gap-2`)}>
+                  <LinkOrSpan item={category} />
+                </div>
+              </li>
+            ))}
+          </ul>
         </li>
-      ))}
-    </ul>
+      </ul>
+    </div>
   );
 };
