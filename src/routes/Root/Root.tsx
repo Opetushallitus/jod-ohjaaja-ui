7dbae6a9e35316790f1dfc3a6ffae4832b5990c6
Sauli Anto <sauli.anto@gofore.com>
2025-11-03 14:33:32 +0200

    OPHJOD-2649: Update DS

--- src/routes/Root/Root.tsx
+++ src/routes/Root/Root.tsx
@@ -171,7 +171,7 @@ const Root = () => {
               language={language as LangCode}
               supportedLanguageCodes={supportedLanguageCodes}
               generateLocalizedPath={generateLocalizedPath}
-              LinkComponent={Link}
+              linkComponent={Link}
               translations={{
                 fi: { change: 'Vaihda kieli.', label: langLabels.fi },
                 sv: { change: 'Andra språk.', label: langLabels.sv },
