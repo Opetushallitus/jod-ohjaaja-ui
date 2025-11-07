a6c13a8798c78e609e45c414d7860c5a3fb0d4f5
Juhani Koivulainen <juhani.koivulainen@elenium.fi>
2025-11-07 14:44:26 +0200

    OPHJOD-1925: Replace custom NavigationList with PageNavigation from DS

--- src/components/MainLayout/ProfileNavigation.tsx
+++ src/components/MainLayout/ProfileNavigation.tsx
@@ -1,35 +1,34 @@
 import { useAppRoutes } from '@/hooks/useAppRoutes';
-import { Accordion, useMediaQueries } from '@jod/design-system';
+import { getLinkTo } from '@/utils/routeUtils';
+import { type MenuSection, PageNavigation, useMediaQueries } from '@jod/design-system';
 import { useTranslation } from 'react-i18next';
-import { NavigationList } from './NavigationList';
+import { useLocation } from 'react-router';
 
 export const ProfileNavigation = () => {
   const {
     t,
     i18n: { language },
   } = useTranslation();
+  const location = useLocation();
   const { lg } = useMediaQueries();
   const { profileRoutes } = useAppRoutes();
-  const navigationListItem = {
-    id: 'index',
-    title: t('profile.index'),
-    path: `/${language}/${t('slugs.profile.index')}/${t('slugs.profile.front')}`,
-    children: profileRoutes.map((route) => ({
-      id: route.path,
-      title: route.name,
-      path: `/${language}/${t('slugs.profile.index')}/${route.path}`,
-    })),
+
+  const menuSection: MenuSection = {
+    title: t('in-this-section'),
+    linkItems: [
+      {
+        label: t('profile.index'),
+        linkComponent: getLinkTo(`/${language}/${t('slugs.profile.index')}/${t('slugs.profile.front')}`),
+        selected: location.pathname === `/${language}/${t('slugs.profile.index')}/${t('slugs.profile.front')}`,
+        collapsed: false,
+        childItems: profileRoutes.map((route) => ({
+          label: route.name,
+          selected: location.pathname.includes(`/${route.path}`),
+          linkComponent: getLinkTo(`/${language}/${t('slugs.profile.index')}/${route.path}`),
+        })),
+      },
+    ],
   };
 
-  return (
-    <div className={`bg-white rounded-lg ${lg ? 'p-6' : 'p-4'}`} data-testid="profile-navigation">
-      {lg ? (
-        <NavigationList rootItem={navigationListItem} menuTitle={t('in-this-section')} />
-      ) : (
-        <Accordion title={t('in-this-section')} initialState={false} data-testid="profile-accordion">
-          <NavigationList rootItem={navigationListItem} />
-        </Accordion>
-      )}
-    </div>
-  );
+  return <PageNavigation menuSection={menuSection} collapsed={!lg} testId="profile-navigation" itemClassname="mb-2" />;
 };
