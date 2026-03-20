93ef493ec06723db8a3277ffe48fe91b5a6df3ec
Sauli Anto <sauli.anto@gofore.com>
2026-03-20 14:35:16 +0200

    OPHJOD-3262: Update Vite to version 8 and related dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'node:url';
 import { defineConfig, loadEnv } from 'vite';
 
