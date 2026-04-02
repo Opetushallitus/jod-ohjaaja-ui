fa845bc54a7b041db9162f51ecb4483c00448762
Sauli Anto <sauli.anto@gofore.com>
2026-04-02 15:40:12 +0300

    OPHJOD-3260: Add Cookie Consent components

--- src/routes/Root/Root.tsx
+++ src/routes/Root/Root.tsx
@@ -14,12 +14,14 @@ import { getLinkTo } from '@/utils/routeUtils';
 import {
   Button,
   Chatbot,
+  CookieConsentProvider,
   Footer,
   LanguageButton,
   MatomoTracker,
   MenuButton,
   NavigationBar,
   SkipLink,
+  useCookieConsent,
   useMediaQueries,
   useNoteStack,
   UserButton,
@@ -212,6 +214,8 @@ const Root = () => {
     });
   }, [addTemporaryNote, t, language]);
 
+  const { open: openCookieConsent } = useCookieConsent();
+
   return (
     <>
       <link rel="manifest" href={`/manifest-${language}.json`} crossOrigin="use-credentials" />
@@ -296,6 +300,8 @@ const Root = () => {
         socialMedia={socialMedia}
         externalLinkIconAriaLabel={t('common:external-link')}
         testId="footer"
+        cookieSettingsLabel={t('common:footer.cookie-settings-label')}
+        onCookieSettingsClick={() => openCookieConsent()}
       />
       <FeedbackModal
         isOpen={feedbackVisible}
@@ -311,4 +317,38 @@ const Root = () => {
   );
 };
 
-export default Root;
+const RootWithCookieConsentProvider = () => {
+  const { t } = useTranslation();
+
+  return (
+    <CookieConsentProvider
+      serviceVariant="yksilo"
+      translations={{
+        guard: {
+          buttonLabel: t('common:cookie-consent.guard.buttonLabel'),
+          description: t('common:cookie-consent.guard.description'),
+          title: t('common:cookie-consent.guard.title'),
+        },
+        modal: {
+          acceptAllLabel: t('common:cookie-consent.modal.acceptAllLabel'),
+          cookiesCategoriesNecessary: t('common:cookie-consent.modal.cookiesCategoriesNecessary'),
+          cookiesCategoriesThirdParty: t('common:cookie-consent.modal.cookiesCategoriesThirdParty'),
+          cookieCategoriesLabel: t('common:cookie-consent.modal.cookieCategoriesLabel'),
+          currentSelectionLabel: t('common:cookie-consent.modal.currentSelectionLabel'),
+          declineOptionalLabel: t('common:cookie-consent.modal.declineOptionalLabel'),
+          description: t('common:cookie-consent.modal.description'),
+          hereLabel: t('common:cookie-consent.modal.hereLabel'),
+          name: t('common:cookie-consent.modal.name'),
+          readMoreHref: t('common:cookie-consent.modal.readMoreHref'),
+          readMoreLabel: t('common:cookie-consent.modal.readMoreLabel'),
+          statisticsDescription: t('common:cookie-consent.modal.statisticsDescription'),
+          title: t('common:cookie-consent.modal.title'),
+        },
+      }}
+    >
+      <Root />
+    </CookieConsentProvider>
+  );
+};
+
+export default RootWithCookieConsentProvider;
